aa8784f2adad758e3bacf681563e6f672c5dd2a6
Mutlu <mutlusims3k@gmail.com>
2024-11-07 12:22:14 +0300

    version import updated

--- frameworks/PerpetualBooster/__init__.py
+++ frameworks/PerpetualBooster/__init__.py
@@ -6,10 +6,6 @@ def setup(*args, **kwargs):
     call_script_in_same_dir(__file__, "setup.sh", *args, **kwargs)
 
 
-def version():
-    return lib_version('perpetual')
-
-
 def run(*args, **kwargs):
     from .exec import run
     return run(*args, **kwargs)
