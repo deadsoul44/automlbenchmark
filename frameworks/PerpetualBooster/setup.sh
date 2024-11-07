0254d5865a922d41bd4be1cf323c0d8812cacab0
Mutlu <mutlusims3k@gmail.com>
2024-11-07 12:06:52 +0300

    fixed setup script

--- frameworks/PerpetualBooster/setup.sh
+++ frameworks/PerpetualBooster/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
-REPO=${2:-"https://github.com/microsoft/FLAML.git"}
-PKG=${3:-"flaml"}
+REPO=${2:-"https://github.com/perpetual-ml/perpetual.git"}
+PKG=${3:-"perpetual"}
 
 
 . ${HERE}/../shared/setup.sh ${HERE} true
 
-PIP install -y perpetual
+PIP install perpetual
