e0f637f1e15d6a5592b4f04c2c1ba58e4defd04f
Mutlu <mutlusims3k@gmail.com>
2024-12-11 18:28:29 +0300

    binary added modified

--- frameworks/PerpetualBooster/exec.py
+++ frameworks/PerpetualBooster/exec.py
@@ -16,7 +16,7 @@ log = logging.getLogger(__name__)
 
 # python3 runbenchmark.py perpetualbooster regression 1h8c_gp3 -m aws -p 3
 # python3 runbenchmark.py autogluon_bestquality openml/t/14949 ag_gp3 -m aws -p 3
-# python3 runbenchmark.py perpetualbooster binary 4h8c_gp3 -m aws -p 3
+# python3 runbenchmark.py perpetualbooster binary_add 4h8c_gp3 -m aws -p 3
 
 
 def run(dataset: Dataset, config: TaskConfig):
