5f9ad8d534dd5a1a596b13d64ebb03a7171077e1
Mutlu <mutlusims3k@gmail.com>
2024-12-22 20:43:21 +0300

    bng mfeat zernike

--- frameworks/PerpetualBooster/exec.py
+++ frameworks/PerpetualBooster/exec.py
@@ -15,7 +15,7 @@ from frameworks.shared.callee import measure_inference_times
 log = logging.getLogger(__name__)
 
 # python3 runbenchmark.py perpetualbooster regression 1h8c_gp3 -m aws -p 3
-# python3 runbenchmark.py autogluon_bestquality openml/t/146045 ag_gp3 -m aws -p 3
+# python3 runbenchmark.py autogluon_bestquality openml/t/146046 ag_gp3 -m aws -p 3
 # python3 runbenchmark.py perpetualbooster binary 4h8c_gp3 -m aws -p 3
 # python3 runbenchmark.py perpetualbooster openml/t/146163 4h8c_gp3 -m aws -p 3
 
